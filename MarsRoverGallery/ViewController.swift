c8fb8683929dbb2ce8f9648aea7556e365973744
ebsamson3 <ebsamson3@gmail.com>
2019-11-04 18:48:22 -0600

    Initial Commit

---
+++ MarsRoverGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarsRoverGallery
+//
+//  Created by Edward Samson on 11/4/19.
+//  Copyright © 2019 Edward Samson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
